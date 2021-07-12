1a8218eb6d6b4d69ce35fd03d27462432381a910
chihiro-f <72483160+chihiro-f@users.noreply.github.com>
2021-07-12 16:43:49 +0900

    'commit7'

--- app/src/main/java/com/webserva/wings/android/pl2_spread/Ranking.java
+++ app/src/main/java/com/webserva/wings/android/pl2_spread/Ranking.java
@@ -36,37 +36,9 @@ public class Ranking extends AppCompatActivity {
         rk_count.setText(rk_str_count);
 
         //ランキングの作成
-<<<<<<< HEAD
-=======
+
         //ランキング情報はサーバから？
-        Room room1 = new Room("room1",1,"19641");
-        Room room2 = new Room("room2",2,"19642");
-        Room room3 = new Room("room3",3,"19643");
-        Room room4 = new Room("room4",4,"19644");
-        Room room5 = new Room("room5",5,"19642");
-        Room room6 = new Room("room6",6,"19642");
-        Room room7 = new Room("room7",7,"19642");
-        Room room8 = new Room("room8",8,"19642");
-        Room room9 = new Room("room9",9,"19642");
-        Room room10 = new Room("room10",10,"19642");
-        Room room11 = new Room("room11",11,"19642");
-        Room room12 = new Room("room12",12,"19642");
 
-        List<Room> list = new ArrayList<>();
-        list.add(room1);
-        list.add(room2);
-        list.add(room3);
-        list.add(room4);
-        list.add(room5);
-        list.add(room6);
-        list.add(room7);
-        list.add(room8);
-        list.add(room9);
-        list.add(room10);
-        list.add(room11);
-        list.add(room12);
-
->>>>>>> bef4770ffef50e5560331aa6bf09e52a1ce159cf
         ListView listview = (ListView) findViewById(R.id.rk_listview_ranking);
 
         RkAdapter rk_adapter = new RkAdapter(this, list);
