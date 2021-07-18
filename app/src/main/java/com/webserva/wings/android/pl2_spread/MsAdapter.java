889494b5427cb314adacf8fb1b8c7090453f4e5c
chihiro-f <72483160+chihiro-f@users.noreply.github.com>
2021-07-18 20:01:30 +0900

    'commit11'

--- app/src/main/java/com/webserva/wings/android/pl2_spread/MsAdapter.java
+++ app/src/main/java/com/webserva/wings/android/pl2_spread/MsAdapter.java
@@ -1,5 +1,6 @@
 package com.webserva.wings.android.pl2_spread;
 
+import android.app.AlertDialog;
 import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -13,16 +14,18 @@ import java.util.List;
 // list_ms.xmlのアダプター
 //Roominfo.xmlに導入
 
-public class MsAdapter extends ArrayAdapter<MemberInfo> {
-    private LayoutInflater mInflater;
+public class MsAdapter extends ArrayAdapter<MemberInfo>{
+    private final LayoutInflater mInflater;
     private TextView list_ms_playername, list_ms_id;
-    private Button mButton;
+    private ListItemButtonClickListener mListener;
 
-    public MsAdapter(Context context, List<MemberInfo> objects) {
-        super(context, 0, objects);
+    public MsAdapter(Context context, List<MemberInfo> objects, ListItemButtonClickListener listener) {
+        super(context,0, objects);
         mInflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        mListener=listener;
     }
 
+    @Override
     public View getView(final int position, View convertView, ViewGroup parent) {
         if (convertView == null) {
             convertView = mInflater.inflate(R.layout.list_ms, null);
@@ -30,15 +33,24 @@ public class MsAdapter extends ArrayAdapter<MemberInfo> {
         final MemberInfo item = this.getItem(position);
         if (item != null) {
             list_ms_playername = (TextView) convertView.findViewById(R.id.list_ms_textview_playername);
+            list_ms_playername.setText(item.getName());
             list_ms_id = (TextView) convertView.findViewById(R.id.list_ms_textview_id);
+            list_ms_id.setText(item.getId());
 
-            mButton = (Button) convertView.findViewById(R.id.list_ms_button_ok);
-            mButton.setOnClickListener(new View.OnClickListener() {
-                public void onClick(View v) {
-                    //承認ボタンをクリックしたときの動作
+            Button button = (Button)convertView.findViewById(R.id.list_ms_button_ok);
+            button.setTag(position);
+            button.findViewById(R.id.list_ms_button_ok).setOnClickListener(new View.OnClickListener(){
+                public void onClick(View v){
+                    //処理内容
+                    mListener.onItemButtonClick(position,v);
                 }
             });
         }
         return convertView;
     }
+
+    interface ListItemButtonClickListener{
+        public void onItemButtonClick(int position, View view);
+    }
+
 }
