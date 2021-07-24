698ff46855ad58331a39674f99fc96f09ae318c3
C5T8fBt-WY <rm715sm426@point.gmobb.jp>
2021-07-24 13:06:56 +0900

    add fullscreen feature

--- app/src/main/java/com/webserva/wings/android/pl2_spread/Game.java
+++ app/src/main/java/com/webserva/wings/android/pl2_spread/Game.java
@@ -2,6 +2,7 @@ package com.webserva.wings.android.pl2_spread;
 
 import android.Manifest;
 import android.app.AlertDialog;
+import android.app.admin.DevicePolicyManager;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -15,13 +16,17 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.os.Looper;
 import android.util.Log;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowInsets;
+import android.view.WindowInsetsController;
+import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.ComponentActivity;
-import androidx.activity.result.ActivityResultLauncher;
-import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.LocationCallback;
@@ -33,8 +38,6 @@ import com.google.android.gms.maps.model.LatLng;
 import java.text.SimpleDateFormat;
 import java.util.Locale;
 
-import static java.lang.System.exit;
-
 public class Game extends ComponentActivity implements SensorEventListener {
     ProgressBar progressBar;
     long time = 3 * 60 * 1000;
@@ -44,6 +47,7 @@ public class Game extends ComponentActivity implements SensorEventListener {
     private Sensor sensor;
     TextView speed, step;
     boolean speedy = false;
+    Button button_full, button_sleep;
 
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -70,7 +74,42 @@ public class Game extends ComponentActivity implements SensorEventListener {
 
         speed = findViewById(R.id.gm_tv_speedvalue);
         step = findViewById(R.id.gm_tv_stepvalue);
+        button_full = findViewById(R.id.gm_button_fullscreen);
+        button_sleep = findViewById(R.id.gm_button_sleep);
 
+        Window w = getWindow();
+
+        button_full.setOnClickListener(v -> {
+            getWindow().addFlags(WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
+            if (Build.VERSION.SDK_INT >= 30) {
+                Log.i("Game", "SDK_30~/" + getWindow().getInsetsController());
+                getWindow().getInsetsController().hide(
+                        WindowInsets.Type.navigationBars() | WindowInsets.Type.statusBars());
+                getWindow().getInsetsController().setSystemBarsBehavior(
+                        WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE);
+                Log.i("Game", getWindow().getInsetsController().getSystemBarsBehavior() + "");
+            } else {
+                Log.i("Game", "NOT_SDK_30~");
+                View decor = getWindow().getDecorView();
+                decor.setSystemUiVisibility(View.SYSTEM_UI_FLAG_HIDE_NAVIGATION | View.SYSTEM_UI_FLAG_FULLSCREEN);
+            }
+        });
+        CountDownTimer cdt = new CountDownTimer(20000, 1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+                if (Build.VERSION.SDK_INT >= 30) {
+                    Log.i("Game", "SDK_30~/" + getWindow().getInsetsController());
+                }
+            }
+
+            @Override
+            public void onFinish() {
+
+            }
+        };
+        new Thread(() -> {
+                cdt.start();
+        }).start();
         action();
     }
 
